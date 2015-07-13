50315f5f9f181496d6448abb707c7b060284447b
Mathieu Méa <mathieu.mea@gmail.com>
2015-07-13 16:49:22 -0400

    Minor improvements...

--- src/org/mtransit/parser/ca_calgary_transit_train/CalgaryTransitTrainAgencyTools.java
+++ src/org/mtransit/parser/ca_calgary_transit_train/CalgaryTransitTrainAgencyTools.java
@@ -39,7 +39,7 @@ public class CalgaryTransitTrainAgencyTools extends DefaultAgencyTools {
 
 	@Override
 	public void start(String[] args) {
-		System.out.printf("\nGenerating Calgary Transit train data...\n");
+		System.out.printf("\nGenerating Calgary Transit train data...");
 		long start = System.currentTimeMillis();
 		this.serviceIds = extractUsefulServiceIds(args, this);
 		super.start(args);
